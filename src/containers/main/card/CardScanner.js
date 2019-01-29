a1a4a4fdacc33b84ba2c0daf9a8eb54ba87bae0f
Jakub Mularski <jakubmularski723@gmail.com>
2019-01-30 00:00:24 +0100

    Before expo eject

---
+++ src/containers/main/card/CardScanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ScannerScreen extends React.Component {
+  navigationOptions = {
+    tabBarVisible: false
+  }
+
+  render() {
+    return null
+  }
+}
