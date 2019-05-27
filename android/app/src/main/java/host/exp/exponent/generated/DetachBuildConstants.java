c76b6005ecf7e3862872c6eb970c9fdbfc6c451c
Jakub Mularski <jakubmularski723@gmail.com>
2019-05-28 00:29:13 +0200

    feat(ci): added expo publish step for master

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp4c23e5709cf8432ca163d61dc7db1323://192.168.0.101:80";
 
 }
