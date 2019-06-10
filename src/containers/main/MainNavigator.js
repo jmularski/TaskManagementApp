676df50f139406eb073142fcf885e9e994f19787
Jakub Mularski <jakubmularski723@gmail.com>
2019-06-10 19:52:30 +0200

    feat(pivot): changed purpose of the app

--- src/containers/main/MainNavigator.js
+++ src/containers/main/MainNavigator.js
@@ -3,12 +3,10 @@ import { createBottomTabNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-import CardScreen from './card/CardNavigator';
 import StatisticsScreen from './Statistics';
 import OptionsScreen from './Options';
 
 const MainNavigator = createBottomTabNavigator({
-  Card: CardScreen,
   Statistics: StatisticsScreen,
   Options: OptionsScreen,
 }, {
@@ -18,9 +16,6 @@ const MainNavigator = createBottomTabNavigator({
       let iconName;
 
       switch (routeName) {
-        case 'Card':
-          iconName = 'md-card';
-          break;
         case 'Statistics':
           iconName = 'md-analytics';
           break;
